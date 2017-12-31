c2ea05342cc6f3d00c12b8885c881d669d3e9b23
Jason Barto <jason.p.barto@gmail.com>
2017-12-31 00:43:38 +0000

    added public / private key signing to AMIs and removed poorly maintained CIS Benchmark from Ansible playbook

--- post_build.sh
+++ post_build.sh
@@ -1,14 +1,30 @@
 #!$(which bash)
 
 IMAGE_IDS=`grep 'artifact,0,id' build.log | cut -d, -f6 | sed 's/%!(PACKER_COMMA)/ /g'`
+echo Grepped ${IMAGE_IDS}
+echo ${PRIVATE_KEY} | base64 -d > tmp_private.pem
+echo ${PRIVATE_KEY}
 
 for id in ${IMAGE_IDS}
 do
+  echo ${id}
   IMAGE_REGION=`echo ${id} | cut -d: -f1`
   IMAGE_ID=`echo ${id} | cut -d: -f2`
-  echo "Recording the creation of image ${IMAGE_ID} in ${IMAGE_REGION}"
+  echo "Preparing to tag image ${IMAGE_ID} in ${IMAGE_REGION}"
 
+  # generate a signature for the AMI ID
+  echo "${IMAGE_REGION}:${IMAGE_ID}" > ami_signature.txt;
+  openssl rsautl -sign -inkey tmp_private.pem -in ami_signature.txt -out ami_signature.sign;
+  # split the signature and write it as a tag to the AMI
+  IMAGE_SIGNATURE=`base64 -i ami_signature.sign`;
+  echo "Created signature ${IMAGE_SIGNATURE}"
+  IMG_SIG_P1=`echo ${IMAGE_SIGNATURE} | cut -b 1-250`;
+  IMG_SIG_P2=`echo ${IMAGE_SIGNATURE} | cut -b 251-500`;
+  aws ec2 create-tags --resources ${IMAGE_ID} --region ${IMAGE_REGION} --tags Key="compliance:signature:part1",Value="${IMG_SIG_P1}" Key="compliance:signature:part2",Value="${IMG_SIG_P2}"
+
+  # record the tagging of the AMI
   echo "\"${IMAGE_REGION}:${IMAGE_ID}\"," >> ami_ids.json
+  echo "Tagged image ${IMAGE_ID} in ${IMAGE_REGION}"
 done
 
 # Packer doesn't return non-zero status; we must do that if Packer build failed
